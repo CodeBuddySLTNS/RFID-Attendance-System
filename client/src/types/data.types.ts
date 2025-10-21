032276fe1a7602fd6ed90a96212ae21b87f1a7e0
Renz Cole <renz2arjhay@gmail.com>
2025-10-21 20:14:50 +0800

    added types

---
+++ client/src/types/data.types.ts
@@ -0,0 +1,11 @@
+export interface Student {
+  rfidTag: string;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  address: string;
+  guardianName: string;
+  departmentId: number;
+  year: number;
+  photo: string;
+}
