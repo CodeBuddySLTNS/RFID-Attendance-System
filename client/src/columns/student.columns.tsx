cdfe7652644aa3dd32c94c168807692f4ad17de1
Renz Cole <renz2arjhay@gmail.com>
2025-10-21 20:40:27 +0800

    update

--- client/src/columns/student.columns.tsx
+++ client/src/columns/student.columns.tsx
@@ -3,27 +3,50 @@ import type { ColumnDef } from "@tanstack/react-table";
 
 export const studentColumns: ColumnDef<Student>[] = [
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: "name",
+    header: "Name",
+    cell: ({ row }) => <div className="capitalize">{row.getValue("name")}</div>,
+  },
+  {
+    accessorKey: "rfidTag",
+    header: () => <div className="text-center">RFID</div>,
     cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("status")}</div>
+      <div className="text-center">{row.getValue("rfidTag")}</div>
     ),
   },
   {
-    accessorKey: "email",
-    header: "Email",
-    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+    accessorKey: "birthDate",
+    header: () => <div className="text-center">Birthdate</div>,
+    cell: ({ row }) => (
+      <div className="text-center">{row.getValue("birthDate")}</div>
+    ),
   },
   {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Amount</div>,
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"));
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount);
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
+    accessorKey: "address",
+    header: () => <div className="text-center">Address</div>,
+    cell: ({ row }) => (
+      <div className="text-center">{row.getValue("address")}</div>
+    ),
+  },
+  {
+    accessorKey: "department",
+    header: () => <div className="text-center">Department</div>,
+    cell: ({ row }) => (
+      <div className="uppercase text-center">{row.getValue("department")}</div>
+    ),
+  },
+  {
+    accessorKey: "year",
+    header: () => <div className="text-center">Year Level</div>,
+    cell: ({ row }) => (
+      <div className="text-center">{row.getValue("year")}</div>
+    ),
+  },
+  {
+    accessorKey: "guradianName",
+    header: () => <div className="text-center">Parent/Guardian</div>,
+    cell: ({ row }) => (
+      <div className="text-center">{row.getValue("guradianName")}</div>
+    ),
   },
 ];
--- client/src/types/data.types.ts
+++ client/src/types/data.types.ts
@@ -1,11 +1,13 @@
 export interface Student {
   rfidTag: string;
+  name: string;
   firstName: string;
   lastName: string;
   birthDate: string;
   address: string;
   guardianName: string;
   departmentId: number;
+  department: string;
   year: number;
   photo: string;
 }
