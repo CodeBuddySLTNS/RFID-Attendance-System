b63a5a51bb6cbba7b4ec42b98b5613905fd66212
Renz Cole <renz2arjhay@gmail.com>
2025-10-21 20:15:01 +0800

    update ui

--- client/src/layout.tsx
+++ client/src/layout.tsx
@@ -1,7 +1,27 @@
 import React, { type ReactNode } from "react";
 
 const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <div className="w-full h-dvh">{children}</div>;
+  return (
+    <div className="w-full h-dvh grid grid-rows-[max-content_1fr]">
+      <div className="w-full p-2 px-3 text-white bg-primary shadow">
+        <div className="flex gap-2">
+          <div className="flex justify-center items-center">
+            <div className="w-10 aspect-square rounded-full bg-gray-200"></div>
+          </div>
+          <div>
+            <h1 className="text-xl font-bold leading-snug">
+              Philippine Advent College Inc. - Salug Campus
+            </h1>
+            <h3 className="leading-tight text-muted">
+              Poblacion East, Salug, Zamboanga del Norte
+            </h3>
+          </div>
+        </div>
+      </div>
+
+      <div>{children}</div>
+    </div>
+  );
 };
 
 export default Layout;
--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -75,23 +75,7 @@ export const Homepage = () => {
   }, []);
 
   return (
-    <div className="w-full h-full grid grid-rows-[max-content_1fr_max-content]">
-      <div className="w-full p-2 px-3 text-white bg-primary shadow">
-        <div className="flex gap-2">
-          <div className="flex justify-center items-center">
-            <div className="w-11 aspect-square rounded-full bg-amber-200"></div>
-          </div>
-          <div>
-            <h1 className="text-xl font-bold leading-snug">
-              Philippine Advent College Inc. - Salug Campus
-            </h1>
-            <h3 className="leading-tight text-muted">
-              Poblacion East, Salug, Zamboanga del Norte
-            </h3>
-          </div>
-        </div>
-      </div>
-
+    <div className="w-full h-full grid grid-rows-[1fr_max-content]">
       <div className="w-full p-3 grid grid-cols-[450px_1fr] gap-3">
         <div className="flex justify-center items-center">
           <div className="w-full h-full grid grid-rows-[40px_1fr_40px] gap-2">
@@ -119,8 +103,8 @@ export const Homepage = () => {
                 key={index}
                 className="w-full h-full grid grid-rows-[40px_1fr] gap-2"
               >
-                <div className="w-full p-1 text-center text-white rounded bg-primary">
-                  <div></div>
+                <div className="w-full flex justify-end items-center p-1 pr-2 text-center font-extrabold text-white rounded bg-primary">
+                  <p className="w-14 border-2 rounded bg-gray-400">-----</p>
                 </div>
 
                 <div className="w-full grid grid-rows-[1fr_max-content] gap-1">
