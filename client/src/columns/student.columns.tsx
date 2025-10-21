5d5e97da276cb18a2425b0b35f03fd734281bcb2
Renz Cole <renz2arjhay@gmail.com>
2025-10-21 20:10:04 +0800

    added datatable

---
+++ client/src/columns/student.columns.tsx
@@ -0,0 +1,29 @@
+import type { Student } from "@/types/data.types";
+import type { ColumnDef } from "@tanstack/react-table";
+
+export const studentColumns: ColumnDef<Student>[] = [
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("status")}</div>
+    ),
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+  },
+];
