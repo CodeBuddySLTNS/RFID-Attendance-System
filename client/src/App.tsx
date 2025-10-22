924a8cad3bf3f9b1d00343797053c746755f47cf
Renz Cole <renz2arjhay@gmail.com>
2025-10-22 15:04:16 +0800

    add route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/homepage";
 import Layout from "./layout";
 import { ManageStudents } from "./pages/manage-students";
+import { AddStudent } from "./pages/add-student";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/manage-students" element={<ManageStudents />} />
+          <Route path="/manage-students/new" element={<AddStudent />} />
         </Routes>
       </Layout>
     </BrowserRouter>
