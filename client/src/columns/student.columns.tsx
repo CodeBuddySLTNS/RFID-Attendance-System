1698a97b77df632253d422b6cc172057ad77a4e5
Renz Cole <renz2arjhay@gmail.com>
2025-10-22 15:03:46 +0800

    update ui

--- client/src/columns/student.columns.tsx
+++ client/src/columns/student.columns.tsx
@@ -1,11 +1,31 @@
 import type { Student } from "@/types/data.types";
 import type { ColumnDef } from "@tanstack/react-table";
+import config from "../../system.config.json";
+
+const url = config.isProduction
+  ? config.prodServer + "/api"
+  : config.devServer + "/api";
 
 export const studentColumns: ColumnDef<Student>[] = [
   {
     accessorKey: "name",
     header: "Name",
-    cell: ({ row }) => <div className="capitalize">{row.getValue("name")}</div>,
+    cell: ({ row }) => {
+      const photo = row.original.photo
+        ? `${url}${row.original.photo}`
+        : "/images/default-icon.png";
+
+      return (
+        <div className="flex items-center gap-2 capitalize">
+          <img
+            className="w-9 aspect-square rounded-full border border-gray-300"
+            src={photo}
+            alt="img"
+          />
+          <span>{row.getValue("name")}</span>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "rfidTag",
@@ -18,7 +38,13 @@ export const studentColumns: ColumnDef<Student>[] = [
     accessorKey: "birthDate",
     header: () => <div className="text-center">Birthdate</div>,
     cell: ({ row }) => (
-      <div className="text-center">{row.getValue("birthDate")}</div>
+      <div className="text-center">
+        {new Date(row.getValue("birthDate")).toLocaleDateString("en-US", {
+          month: "short",
+          day: "2-digit",
+          year: "numeric",
+        })}
+      </div>
     ),
   },
   {
@@ -43,10 +69,10 @@ export const studentColumns: ColumnDef<Student>[] = [
     ),
   },
   {
-    accessorKey: "guradianName",
+    accessorKey: "guardianName",
     header: () => <div className="text-center">Parent/Guardian</div>,
     cell: ({ row }) => (
-      <div className="text-center">{row.getValue("guradianName")}</div>
+      <div className="text-center">{row.getValue("guardianName")}</div>
     ),
   },
 ];
--- client/src/pages/manage-students.tsx
+++ client/src/pages/manage-students.tsx
@@ -1,9 +1,30 @@
 import DataTablePagination from "@/components/datatable";
+import { Button } from "@/components/ui/button";
+import { coleAPI } from "@/lib/utils";
+import { type Student } from "@/types/data.types";
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
 
 export const ManageStudents = () => {
+  const { data: students } = useQuery<Student[]>({
+    queryKey: ["students"],
+    queryFn: coleAPI("/students"),
+  });
+
   return (
     <div>
-      <DataTablePagination data={[]} />
+      <div className="px-6 flex items-center justify-between mt-4">
+        <h2 className="font-bold text-xl border-l-5 border-orange-500 pl-2">
+          Manage Students
+        </h2>
+
+        <div>
+          <Link to="/manage-students/new">
+            <Button size="sm">Add Student</Button>
+          </Link>
+        </div>
+      </div>
+      <DataTablePagination data={students || []} />
     </div>
   );
 };
